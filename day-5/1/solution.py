cc913c145342bf6cc56ec81909a826fcabb0f5a1
bilith <lukasdyduch@gmail.com>
2023-12-05 18:48:45 +0100

    Day 5 progress

--- day-5/1/solution.py
+++ day-5/1/solution.py
@@ -1,17 +1,37 @@
 def solve(input: str) -> int:
     parts = [line.strip() for line in input.strip().split('\n\n')]
-    seeds = dict.fromkeys(parts[0].split(': ')[1].split())
-    print(seeds)
+    seeds = [[int(seed)] for seed in parts[0].split(': ')[1].split()]
     maps = parts[1:]
 
-    for m in maps:
-        print(m)
+    for i, m in enumerate(maps):
+        for line in m.splitlines():
+            if ' map:' in line:
+                continue
 
-    return 0
+            dest, source, r = [int(n) for n in line.split()]
+            
+            for x, seed in enumerate(seeds):
+                print(f'checking seed "{seed[-1]}" for line "{line}" - seeds {seeds}')
+
+                if len(seed) >= i+2:
+                    continue
+
+                if seed[-1] >= source and seed[-1] < (source + r):
+                    seed.append(dest + seed[-1] - source)
+                    print(f'append #1 ({x}) seed {seed}: {dest + seed[-1] - source}')
+        
+        for seed in seeds:
+            if len(seed) < i+2:
+                seed.append(seed[-1])
+                print(f'append #2 seed {seed}: {seed[-1]}')
+
+        print(seeds)
+        print(f'-- finished processing map #{i} --')
+
+    return min([s[-1] for s in seeds])
 
 def test():
-    input = '''
-seeds: 79 14 55 13
+    input = '''seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -49,5 +69,5 @@ humidity-to-location map:
     print(result)
     assert result == 35
 
-test()
-# print(solve(open('input.txt').read()))
+# test()
+print(solve(open('input.txt').read()))
